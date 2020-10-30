a25637fdf29dad9bb3680e4a304c46fd7b3dfdd2
ellelee0305 <72188881+ellelee0305@users.noreply.github.com>
2020-10-31 00:45:43 +0800

    Update waveform.py
    sigh again

--- PyHipp/waveform.py
+++ PyHipp/waveform.py
@@ -1,5 +1,9 @@
 import DataProcessingTools as DPT
 import matplotlib.pyplot as plt
+import hickle as hkl
+import os
+import numpy as np
+
 
 class Waveform(DPT.DPObject):
     # Please change the class name according to your needs
@@ -42,10 +46,20 @@ class Waveform(DPT.DPObject):
         # ..................code...................
         # .........................................
         
-        
+        pwd = os.path.normpath(os.getcwd());
+        # 'channelxxx, xxx is the number of the channel'
+        self.channel_filename = [os.path.basename(pwd)]  
+        template_filename = os.path.join(
+        DPT.levels.resolve_level('day', self.channel_filename[0]),
+        'mountains', self.channel_filename[0], 'output', 'templates.hkl')
+
+        templates = hkl.load(template_filename)
+
+        self.data = [np.squeeze(templates)]
+
         # check on the mountainsort template data and create a DPT object accordingly
         # Example:
-        if <data-is-not-empty>:
+        if self.data:
             # create object if data is not empty
             DPT.DPObject.create(self, *args, **kwargs)
         else:
